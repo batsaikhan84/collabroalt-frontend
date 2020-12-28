01590b5d81c71ba5092c64fd6ee3276ec275dda0
Batsaikhan Ulambayar <u.batsaikhan@gmail.com>
2020-12-27 19:14:38 -0600

    date pipe

--- angular/demo/src/app/employees/employees.component.ts
+++ angular/demo/src/app/employees/employees.component.ts
@@ -7,11 +7,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EmployeesComponent implements OnInit {
   title = "Employee List"
+  
   employees = [
-    {id: 101, name: "Jon Snow", email: "snow@email.com", isActive: true},
-    {id: 102, name: "Kim Uh", email: "uh@email.com", isActive: true},
-    {id: 103, name: "Sara Romo", email: "romo@email.com", isActive: false}
+    {id: 101, name: "Jon Snow", email: "snow@email.com", isActive: true, department: 'marketing', hireDate: "1/25/2018", annualSalary: "80000"},
+    {id: 102, name: "Kim Uh", email: "uh@email.com", isActive: true, department: 'accounting', hireDate: "1/19/2011", annualSalary: "85000"},
+    {id: 103, name: "Sara Romo", email: "romo@email.com", isActive: false, department: 'IT', hireDate: "6/2/2015", annualSalary: "75000"},
+    {id: 104, name: "Rob Thomas", email: "thomas@email.com", isActive: false, department: 'R&D', hireDate: "4/29/2008", annualSalary: "90000"}
   ]
+  columnNames = this.employees[0]
   constructor() { }
 
   ngOnInit(): void {
@@ -26,9 +29,13 @@ export class EmployeesComponent implements OnInit {
   buttonClass(employee: any) {
     if (employee.isActive === true) {
       return 'active-button'
-    } else if (employee.isActive === false) {
+    } else {
       return 'inActive-button'
     }
   }
+  // employeeKeys() {
+  //   this.employees[0].map(employee => {
 
+  //   })
+  // }
 }

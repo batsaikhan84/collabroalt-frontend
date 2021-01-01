ad24053328543a5b735e6c863f2b5383e63e3c88
Batsaikhan Ulambayar <u.batsaikhan@gmail.com>
2021-01-01 15:55:28 -0600

    implemented timer

--- angular/projectOne/src/app/components/timer/timer.component.ts
+++ angular/projectOne/src/app/components/timer/timer.component.ts
@@ -1,18 +1,40 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { interval, Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-timer',
   templateUrl: './timer.component.html',
   styleUrls: ['./timer.component.css']
 })
-export class TimerComponent implements OnInit {
+export class TimerComponent implements OnInit, OnDestroy {
   count = 0
+  isTimerOn = false
+  private timerSubscription: any
   constructor() { 
-  
   }
-
   ngOnInit(): void {
-    
+    console.log(this.isTimerOn)
   }
-
+  incrementCount() {
+      this.count++
+  }
+  decrementCount() {
+    this.count--
+  }
+  resetTimer() {
+    this.count = 0
+  }
+  startTimer() {
+    if (!this.isTimerOn) {
+      this.timerSubscription = interval(1000).subscribe( () => this.incrementCount())
+      this.isTimerOn = true
+    }
+  }
+  stopTimer() {
+    this.ngOnDestroy()
+    this.isTimerOn = false
+  }
+  ngOnDestroy(): void {
+    this.timerSubscription.unsubscribe()
+  }
 }

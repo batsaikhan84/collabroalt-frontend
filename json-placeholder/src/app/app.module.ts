c70bcf983cf7cedbd71b49661c0eb9bd408e7c80
Batsaikhan Ulambayar <u.batsaikhan@gmail.com>
2021-01-07 14:17:27 -0600

    create user component

--- json-placeholder/src/app/app.module.ts
+++ json-placeholder/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
+import { UserComponent } from './components/user/user.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    UserComponent
   ],
   imports: [
     BrowserModule,

cc8ec824b4770c167ad9cd172ed1b89f52f86cee
Batsaikhan Ulambayar <u.batsaikhan@gmail.com>
2021-01-14 11:24:37 -0600

    create home header products components

--- productManagement/src/app/app.module.ts
+++ productManagement/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { HomeComponent } from './components/home/home.component';
+import { ProductsComponent } from './components/products/products.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    HomeComponent,
+    ProductsComponent
   ],
   imports: [
     BrowserModule,

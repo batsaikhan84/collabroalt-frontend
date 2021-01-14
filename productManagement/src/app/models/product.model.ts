43e1dfd5a7db1da7e45071bc64fd84cd47fc3108
Batsaikhan Ulambayar <u.batsaikhan@gmail.com>
2021-01-14 11:47:56 -0600

    create product model

---
+++ productManagement/src/app/models/product.model.ts
@@ -0,0 +1,9 @@
+export interface Product {
+    id: number;
+    title: String;
+    description: String;
+    imageLink: String;
+    shipping: String;
+    price: number;
+    availability: string;
+}
